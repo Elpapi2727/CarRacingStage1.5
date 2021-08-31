4555667f6bc5013f65ef7743e6fd1399a8b7a21d
Elpapi2727 <85907374+Elpapi2727@users.noreply.github.com>
2021-08-31 17:51:53 -0500

    l

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ var playerCount;
 var allPlayers;
 var distance = 0;
 var database;
+var car1img, car2img, car3img, car4img;
+var trackimg;
 
 var form, player, game;
 
@@ -19,13 +21,23 @@ function setup(){
   game.start();
 }
 
+function preload(){
+  car1img = loadImage("images/car1.png");
+  car2img = loadImage("images/car2.png");
+  car3img = loadImage("images/car3.png");
+  car4img = loadImage("images/car4.png");
+  trackimg = loadImage("images/track.jpg");
+}
 
 function draw(){
-  if(playerCount === 4){
+  if(playerCount === 1){
     game.update(1);
   }
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if (gameState === 2){
+game.end();
   }
 }
